6ef5e026cc3d2cf87ea97d6edebedb8d18fdee3a
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-07-21 16:51:55 -0400

    minor updates

---
+++ client/build/precache-manifest.61cbcfae68c101ac054641d146731b38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be2701c5404691f253c951c14ad9af4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bdf22bb82a0ecb7861d",
+    "url": "/static/css/main.3812826f.chunk.css"
+  },
+  {
+    "revision": "a3d84bf1b7b6b3fc468f",
+    "url": "/static/js/2.9f2bf9f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9f2bf9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bdf22bb82a0ecb7861d",
+    "url": "/static/js/main.a70377e8.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "fea627fed152682ba93427a58ef3d277",
+    "url": "/static/media/Jordi_01.fea627fe.png"
+  },
+  {
+    "revision": "5a12b23d9572b56cb6a44d2bb5b2fca5",
+    "url": "/static/media/hexagonbackground.5a12b23d.png"
+  }
+]);
