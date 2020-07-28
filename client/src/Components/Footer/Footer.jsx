e80f21e2443815e050370b6d8bef8c767ba8b6d3
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-07-28 11:21:16 -0400

    footer links uodate

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -19,9 +19,125 @@ function Footer() {
           <i className="fas fa-chevron-up"></i>
         </div>
         <p>&copy; 2020 Jordania Pereyra</p>
+
+        <section>
+          <ul class="icon-list">
+            <li class="icon-item">
+              <a
+                href="https://github.com/JordisGithub"
+                className="footer-icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-github-square"></i>
+              </a>
+            </li>
+            <li class="icon-item">
+              <a
+                href="https://www.linkedin.com/in/jordania-pereyra/"
+                className="footer-icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+            <li class="icon-item">
+              <a
+                href="https://www.instagram.com/jordi_515/"
+                className="footer-icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li class="icon-item">
+              <a
+                href="#"
+                className="footer-icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="icon-item">
+              <a
+                href="#"
+                className="footer-icon-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+          </ul>
+        </section>
       </footer>
     </>
   );
 }
 
 export default Footer;
+
+{
+  /* <div className="social-media">
+  <ul className="about-nav-list">
+    <li>
+      <a
+        href="https://github.com/JordisGithub"
+        className="icon-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-github-square"></i>
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://www.linkedin.com/in/jordania-pereyra/"
+        className="icon-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-linkedin"></i>
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://www.instagram.com/jordi_515/"
+        className="icon-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fab fa-instagram"></i>
+      </a>
+    </li> */
+}
+{
+  /* <li>
+                  <a
+                    href="https://twitter.com/jordip515?lang=en"
+                    className="icon-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                </li> */
+}
+{
+  /* 
+                <li>
+                  <a
+                    href="https://www.facebook.com/jordania.pereyra"
+                    className="icon-link"
+                    target="_blank"
+                  >
+                    <i className="fab fa-facebook-square"></i>
+                  </a>
+                </li> */
+}
+//   </ul>
+// </div>;
