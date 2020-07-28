79e3566bf7703d3d2d2b7eab3b19499e17ec5bf1
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-07-27 22:41:32 -0400

    about me css update

--- client/src/App.js
+++ client/src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
         <Header />
         <TheStack />
         <Specializations />
-        {/* <AboutMe /> */}
+        <AboutMe />
         <MyWork />
         <Experience />
         <ContactForm />
